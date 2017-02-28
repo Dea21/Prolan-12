b0f46a3fa938815eb2dbf7f2f60589b4b6756237
Muhammad Bandung Radea 10115308 <muhammadtheradea@gmail.com>
2017-02-28 18:51:45 +0700

    Add files via upload

---
+++ Minggu ke 2/Mahasiswa.java
@@ -0,0 +1,7 @@
+class Mahasiswa{
+	public static void main(String argv[]){
+		System.out.println("Nama Lengkap:Muhammad Bandung Radea");
+		System.out.println("NIM:10115308");
+		System.out.println("Alamat:Jl. Sasak Gantung IV,Bandung");
+	}
+}
