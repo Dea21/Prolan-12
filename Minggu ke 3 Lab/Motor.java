f85ad828458a490f6745fb0d709418fe8db8344d
Muhammad Bandung Radea 10115308 <muhammadtheradea@gmail.com>
2017-03-08 19:59:20 +0700

    Add files via upload

---
+++ Minggu ke 3 Lab/Motor.java
@@ -0,0 +1,13 @@
+public class Motor {
+
+    
+    public static void main(String[] args) {
+        data honda = new data();
+        honda.nama="Honda";
+        honda.ban="Michelin";
+        honda.oli="Repsol";
+		honda.jenis="Motor Bebek";
+        honda.info();
+    }
+    
+}
---
+++ Minggu ke 3 Lab/data.java
@@ -0,0 +1,11 @@
+public class data {
+    public String nama,ban,oli,jenis;
+    
+    public void info(){
+        System.out.println("Info:");
+        System.out.println("Nama "+ nama);
+        System.out.println("Ban "+ ban);
+        System.out.println("oli "+ oli);
+		System.out.println("jenis "+ jenis);
+    }
+}
